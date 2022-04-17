51b34b800f3063fbe79d2c35f173d9422316788e
Charles Carway <78627703+charlie-carway@users.noreply.github.com>
2022-04-16 22:12:29 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -139,8 +139,8 @@ def ping(host, timeout=1):
                                                   ## added END
 
     
-    #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
-    #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]                ## added line
+    #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)), str(round(stdev(stdev_var), 8))]
+    vars = [int(round(packet_min, 8)), int(round(packet_avg, 8)), int(round(packet_max, 8)), int(round(stdev(stdev_var), 8))]                ## added line
 
     return vars
 
